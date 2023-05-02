ccf63c8ede380af7fafa0abad901c25e83e25a4d
dhyces <pokmon987@gmail.com>
2023-05-02 14:27:58 -0400

    Port to 1.19.4

--- Forge/src/main/java/dhyces/waxablecoral/ForgeWaxableCoralClient.java
+++ Forge/src/main/java/dhyces/waxablecoral/ForgeWaxableCoralClient.java
@@ -1,24 +1,23 @@
 package dhyces.waxablecoral;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
+import com.mojang.blaze3d.vertex.*;
 import dhyces.waxablecoral.registration.RegistryObject;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.IItemDecorator;
 import net.minecraftforge.client.event.RegisterItemDecorationsEvent;
+import net.minecraftforge.common.util.MutableHashedLinkedMap;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.function.Supplier;
 
 public class ForgeWaxableCoralClient {
     static void init(IEventBus modBus) {
@@ -33,63 +32,86 @@ public class ForgeWaxableCoralClient {
 
     private static void addTabs(final CreativeModeTabEvent.BuildContents event) {
         if (event.getTab() == CreativeModeTabs.NATURAL_BLOCKS) {
-            event.accept(Register.WAXED_TUBE_CORAL_BLOCK);
-            event.accept(Register.WAXED_BRAIN_CORAL_BLOCK);
-            event.accept(Register.WAXED_BUBBLE_CORAL_BLOCK);
-            event.accept(Register.WAXED_FIRE_CORAL_BLOCK);
-            event.accept(Register.WAXED_HORN_CORAL_BLOCK);
+            MutableHashedLinkedMap<ItemStack, CreativeModeTab.TabVisibility> tabItems = event.getEntries();
 
-            event.accept(Register.WAXED_TUBE_CORAL);
-            event.accept(Register.WAXED_BRAIN_CORAL);
-            event.accept(Register.WAXED_BUBBLE_CORAL);
-            event.accept(Register.WAXED_FIRE_CORAL);
-            event.accept(Register.WAXED_HORN_CORAL);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_HORN_CORAL_FAN);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_FIRE_CORAL_FAN);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_BUBBLE_CORAL_FAN);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_BRAIN_CORAL_FAN);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_TUBE_CORAL_FAN);
 
-            event.accept(Register.WAXED_TUBE_CORAL_FAN);
-            event.accept(Register.WAXED_BRAIN_CORAL_FAN);
-            event.accept(Register.WAXED_BUBBLE_CORAL_FAN);
-            event.accept(Register.WAXED_FIRE_CORAL_FAN);
-            event.accept(Register.WAXED_HORN_CORAL_FAN);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_HORN_CORAL);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_FIRE_CORAL);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_BUBBLE_CORAL);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_BRAIN_CORAL);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_TUBE_CORAL);
+
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_HORN_CORAL_BLOCK);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_FIRE_CORAL_BLOCK);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_BUBBLE_CORAL_BLOCK);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_BRAIN_CORAL_BLOCK);
+            putAfterFullVis(tabItems, Items.DEAD_HORN_CORAL_FAN, Register.WAXED_TUBE_CORAL_BLOCK);
         }
+    }
+
+    private static void putAfterFullVis(MutableHashedLinkedMap<ItemStack, CreativeModeTab.TabVisibility> tabItems, Item item, Supplier<? extends Block> regObj) {
+        tabItems.putAfter(item.getDefaultInstance(), regObj.get().asItem().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
     }
 
     private static void registerItemDecorator(RegisterItemDecorationsEvent event) {
         for (RegistryObject<Item> item : Register.ITEM.getEntries()) {
-            event.register(item.get(), ForgeWaxableCoralClient::renderWaxedItemDecorator);
+            event.register(item.get(), WaxedIconRenderer.INSTANCE);
         }
-        event.register(Items.WAXED_COPPER_BLOCK, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_CUT_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_CUT_COPPER_STAIRS, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_CUT_COPPER_SLAB, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_EXPOSED_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_EXPOSED_CUT_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_EXPOSED_CUT_COPPER_STAIRS, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_EXPOSED_CUT_COPPER_SLAB, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_WEATHERED_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_WEATHERED_CUT_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_WEATHERED_CUT_COPPER_STAIRS, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_WEATHERED_CUT_COPPER_SLAB, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_OXIDIZED_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_OXIDIZED_CUT_COPPER, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_OXIDIZED_CUT_COPPER_STAIRS, ForgeWaxableCoralClient::renderWaxedItemDecorator);
-        event.register(Items.WAXED_OXIDIZED_CUT_COPPER_SLAB, ForgeWaxableCoralClient::renderWaxedItemDecorator);
+        event.register(Items.WAXED_COPPER_BLOCK, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_CUT_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_CUT_COPPER_STAIRS, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_CUT_COPPER_SLAB, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_EXPOSED_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_EXPOSED_CUT_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_EXPOSED_CUT_COPPER_STAIRS, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_EXPOSED_CUT_COPPER_SLAB, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_WEATHERED_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_WEATHERED_CUT_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_WEATHERED_CUT_COPPER_STAIRS, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_WEATHERED_CUT_COPPER_SLAB, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_OXIDIZED_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_OXIDIZED_CUT_COPPER, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_OXIDIZED_CUT_COPPER_STAIRS, WaxedIconRenderer.INSTANCE);
+        event.register(Items.WAXED_OXIDIZED_CUT_COPPER_SLAB, WaxedIconRenderer.INSTANCE);
     }
 
     public static final ResourceLocation WAXED_ICON = WaxableCoral.id("textures/gui/waxed_icon.png");
 
-    private static boolean renderWaxedItemDecorator(Font font, ItemStack stack, int xOffset, int yOffset, float blitOffset) {
-        if (!(Minecraft.getInstance().screen != null && Minecraft.getInstance().screen instanceof AbstractContainerScreen<?> containerScreen && containerScreen.getMenu().getCarried() == stack)) {
-            RenderSystem.setShaderTexture(0, WAXED_ICON);
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            Tesselator tesselator = Tesselator.getInstance();
-            BufferBuilder builder = tesselator.getBuilder();
-            builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-            builder.vertex(xOffset+8, yOffset, blitOffset+200).uv(0, 0).endVertex();
-            builder.vertex(xOffset+8, yOffset+8, blitOffset+200).uv(0, 1).endVertex();
-            builder.vertex(xOffset+16, yOffset+8, blitOffset+200).uv(1, 1).endVertex();
-            builder.vertex(xOffset+16, yOffset, blitOffset+200).uv(1, 0).endVertex();
-            tesselator.end();
+    private static final class WaxedIconRenderer implements IItemDecorator {
+        public static final WaxedIconRenderer INSTANCE = new WaxedIconRenderer();
+        
+        private WaxedIconRenderer() {}
+        
+        @Override
+        public boolean render(PoseStack poseStack, Font font, ItemStack stack, int xOffset, int yOffset) {
+            if (!isItemMouseCarried(stack)) {
+                RenderSystem.setShaderTexture(0, WAXED_ICON);
+                RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                Tesselator tesselator = Tesselator.getInstance();
+                BufferBuilder builder = tesselator.getBuilder();
+                poseStack.pushPose();
+                poseStack.translate(xOffset+8, yOffset, 200);
+                poseStack.scale(8, 8, 8);
+                PoseStack.Pose pose = poseStack.last();
+                builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+                builder.vertex(pose.pose(), 0, 0, 0).uv(0, 0).endVertex();
+                builder.vertex(pose.pose(), 0, 1, 0).uv(0, 1).endVertex();
+                builder.vertex(pose.pose(), 1, 1, 0).uv(1, 1).endVertex();
+                builder.vertex(pose.pose(), 1, 0, 0).uv(1, 0).endVertex();
+                poseStack.popPose();
+                tesselator.end();
+                return true;
+            }
+            return false;
         }
-        return true;
+
+        private boolean isItemMouseCarried(ItemStack stack) {
+            return Minecraft.getInstance().screen != null && Minecraft.getInstance().screen instanceof AbstractContainerScreen<?> containerScreen && containerScreen.getMenu().getCarried() == stack;
+        }
     }
 }
